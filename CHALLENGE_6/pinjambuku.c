5bdfc8eab138fb33ff36b62e1279a0447386af10
Dezkrazzer <uqie.vanmadiun@gmail.com>
2025-09-22 15:04:48 +0700

    feat: challenge_6

---
+++ CHALLENGE_6/pinjambuku.c
@@ -0,0 +1,52 @@
+// Program to manage book borrowing in a library
+
+/*
+    SOAL:
+    suatu hari, seseorang meminjam buku.
+    Diberikan hari ke-(N) orang tersebut mengembalikan buku => N (integer positive)
+    Diberikan hari ke-(M) orang tersebut harus mengembalikan buku => M (integer positive)
+    Diberikan sanksi denda Rp.(D) per hari, jika orang tersebut melewati batas hari pengembalian => D (integer positive)
+    jika orang tersebut mengembalikan buku sebelum/tepat waktu batas hari pengembalian, maka orang tersebut tidak diberikan denda.
+    jika orang tersebut mengembalikan buku melewati batas hari pengembalian, maka orang tersebut akan diberikan denda
+
+    Input Format:
+    N M D
+
+    Constraints:
+    1000 <= D <= 100000
+    7 <= M <= 10000
+    1 <= N <= M*2
+
+    Output Format:
+    apakah tepat waktu ? "TEPAT WAKTU" : Total kumulatif Denda
+
+    Sample Input 0:
+    10 20 4000
+
+    Sample Output 0:
+    TEPAT WAKTU
+
+    Sample Input 1:
+    35 32 5000
+
+    Sample Output 1:
+    15000
+*/
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int hari_kembali, hari_batas, denda_per_hari;
+    scanf("%d %d %d", &hari_kembali, &hari_batas, &denda_per_hari);
+
+    if(hari_kembali <= hari_batas){
+        printf("TEPAT WAKTU\n");
+    } else {
+        int total_denda_yang_diberikan = (hari_kembali - hari_batas) * denda_per_hari;
+        printf("%d\n", total_denda_yang_diberikan);
+    }
+
+    return 0;
+}

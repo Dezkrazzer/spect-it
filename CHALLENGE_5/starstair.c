9175557b24441b14158c2e110cc78fca31da170c
Dezkrazzer <uqie.vanmadiun@gmail.com>
2025-09-22 14:58:21 +0700

    feat: challenge_5

---
+++ CHALLENGE_5/starstair.c
@@ -0,0 +1,55 @@
+// Program to print star stair pattern
+
+/*
+    SOAL:
+    Diberikan sebuah Singel Integer positif, N. berikan sebuah pola segitiga siku-siku dengan tinggi dan alas sepanjang N.
+    baris pertama berisi 1 karakter bintang
+    baris kedua berisi 2 karakter bintang
+    ...
+    baris ke-N berisi N karakter bintang
+
+    Input Format:
+    N (integer positif)
+
+    Constraints:
+    1 <= N <= 1000
+
+    Output Format:
+    pola bintang(*), tanpa spasi di antaranya
+
+    Sample Input 0:
+    3
+
+    Sample Output 0:
+    *
+    **
+    ***
+
+    Sample Input 1:
+    5
+
+    Sample Output 1:
+    *
+    **
+    ***
+    ****
+    *****
+*/
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int masukan_angka;
+    scanf("%d", &masukan_angka);
+
+    for(int baris = 1; baris <= masukan_angka; baris++){
+        for(int kolom = 1; kolom <= baris; kolom++){ 
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
